23523b8f75a874bedfd526f035bfd69e7bb549da
saurav mishra <saurav12.908@gmail.com>
2021-01-22 01:41:20 +0530

    minor fixes

--- src/OneStepTowardsChange/views.py
+++ src/OneStepTowardsChange/views.py
@@ -2,4 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render 
 
 def home(request):
-    return render(request, 'home_page.html',{})
+    context = {
+        
+    }
+    
+    return render(request, 'home_page.html',context)
